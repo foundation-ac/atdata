7c6985968a7c6aebb6a31fa0833aba9941e6518f
Maxine Levesque <170461181+maxinelevesque@users.noreply.github.com>
2025-10-23 13:39:06 -0700

    Updated tests to expand coverage

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -102,12 +102,15 @@ def test_wds(
         ):
     """Test our ability to write samples as `WebDatasets` to disk"""
 
+    ## Testing hyperparameters
+
     n_copies = 100
     shard_maxcount = 10
     batch_size = 4
     n_iterate = 10
 
-    # Write sharded dataset
+
+    ## Write sharded dataset
 
     file_pattern = (
         tmp_path
@@ -127,7 +130,95 @@ def test_wds(
 
             sink.write( new_sample.as_wds )
     
-    # Read first shard
+
+    ## Ordered
+
+    # Read first shard, no batches
+
+    first_filename = file_pattern.format( shard_id = f'{0:06d}' )
+    dataset = atdata.Dataset[SampleType]( first_filename )
+
+    iterations_run = 0
+    for i_iterate, cur_sample in enumerate( dataset.ordered( batch_size = None ) ):
+        
+        assert isinstance( cur_sample, SampleType ), \
+            f'Single sample for {SampleType} written to `wds` is of wrong type'
+        
+        # Check sample values
+        
+        for k, v in sample_data.items():
+            if isinstance( v, np.ndarray ):
+                is_correct = np.all( getattr( cur_sample, k ) == v )
+            else:
+                is_correct = getattr( cur_sample, k ) == v
+            assert is_correct, \
+                f'{SampleType}: Incorrect sample value found for {k}'
+
+        iterations_run += 1
+        if iterations_run >= n_iterate:
+            break
+
+    assert iterations_run == n_iterate, \
+        f"Only found {iterations_run} samples, not {n_iterate}"
+
+    # Read all shards, batches
+
+    start_id = f'{0:06d}'
+    end_id = f'{9:06d}'
+    first_filename = file_pattern.format( shard_id = '{' + start_id + '..' + end_id + '}' )
+    print( first_filename )
+    dataset = atdata.Dataset[SampleType]( first_filename )
+
+    iterations_run = 0
+    for i_iterate, cur_batch in enumerate( dataset.ordered( batch_size = batch_size ) ):
+        
+        assert isinstance( cur_batch, atdata.SampleBatch ), \
+            f'{SampleType}: Batch sample is not correctly a batch'
+        
+        assert cur_batch.sample_type == SampleType, \
+            f'{SampleType}: Batch `sample_type` is incorrect type'
+        
+        if i_iterate == 0:
+            cur_n = len( cur_batch.samples )
+            assert cur_n == batch_size, \
+                f'{SampleType}: Batch has {cur_n} samples, not {batch_size}'
+        
+        assert isinstance( cur_batch.samples[0], SampleType ), \
+            f'{SampleType}: Batch sample of wrong type ({type( cur_batch.samples[0])})'
+        
+        # Check batch values
+        for k, v in sample_data.items():
+            cur_batch_data = getattr( cur_batch, k )
+
+            if isinstance( v, np.ndarray ):
+                assert isinstance( cur_batch_data, np.ndarray ), \
+                    f'{SampleType}: `NDArray` not carried through to batch'
+                
+                is_correct = all( 
+                    [ np.all( cur_batch_data[i] == v )
+                      for i in range( cur_batch_data.shape[0] ) ]
+                )
+
+            else:
+                is_correct = all( 
+                    [ cur_batch_data[i] == v
+                      for i in range( len( cur_batch_data ) ) ]
+                )
+
+            assert is_correct, \
+                f'{SampleType}: Incorrect sample value found for {k}'
+
+        iterations_run += 1
+        if iterations_run >= n_iterate:
+            break
+
+    assert iterations_run == n_iterate, \
+        "Only found {iterations_run} samples, not {n_iterate}"
+    
+
+    ## Shuffled
+
+    # Read first shard, no batches
 
     first_filename = file_pattern.format( shard_id = f'{0:06d}' )
     dataset = atdata.Dataset[SampleType]( first_filename )
@@ -145,7 +236,7 @@ def test_wds(
     assert iterations_run == n_iterate, \
         f"Only found {iterations_run} samples, not {n_iterate}"
 
-    # Read batches
+    # Read all shards, batches
 
     start_id = f'{0:06d}'
     end_id = f'{9:06d}'
